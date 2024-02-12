66609fc929c28d1a92242a8c434708e1b881fd30
sara pena <sarap@Saras-MacBook-Air.local>
2024-02-11 22:44:17 -0800

    Completed Assignment

--- public/index.js
+++ public/index.js
@@ -1,19 +1,47 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter')
+    let responseJSON = await response.json()
+    console.log(responseJSON)
+    let countValue = responseJSON.value;
 
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
+   async function increment(){
+        countValue++; 
+    let incrementResponse = await fetch('http://localhost:9001/counter', {
+        method: 'PATCH' ,
+        headers: {
+            'Content-Type' : 'application/json'
+   },
+   body: JSON.stringify({
+    value: countValue
+   })
+})
+
+   let incrementResponseJSON = await incrementResponse.json()
+
+        countContainer.textContent = incrementResponseJSON.value;
     }
 
-    function decrement(){
+    async function decrement(){
         countValue--;
-        countContainer.textContent = countValue;
+        let decrementResponse = await fetch('http://localhost:9001/counter', {
+            method: 'PATCH' ,
+            headers: {
+                'Content-Type' : 'application/json'
+       },
+       body: JSON.stringify({
+        value: countValue
+       })
+    })
+    
+       let decrementResponseJSON = await decrementResponse.json()
+
+       countContainer.textContent = decrementResponseJSON.value;
     }
+
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
